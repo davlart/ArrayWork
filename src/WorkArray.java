19d05bb556878664ce1a39522de2987a4526479d
ArthurArt <davlartprivate@mail.ru>
2017-01-11 11:53:54 +0300

    second work

--- src/WorkArray.java
+++ src/WorkArray.java
@@ -57,6 +57,46 @@ public static void t1(int[] myArray)
                 System.out.print( sum);
     }
 
+    public static void math1 (){
+        int a = Integer.parseInt(sc.nextLine()),
+                b = Integer.parseInt(sc.nextLine()),
+                c = Integer.parseInt(sc.nextLine()),
+                d = Integer.parseInt(sc.nextLine()), m,n,m1,n1;
+        m=a*d+c*b; n=d*b; m1=m; n1=n;
+        while(m1!=n1){
+            if(m1>n1) m1 -=n1;
+            else n1 -=m1;
+        }
+        m=m/m1; n=n/m1;
+        System.out.format("%d %d",m,n);
+    }
+
+    public static void math2 (){
+        int n=Integer.parseInt(sc.nextLine()),m=Integer.parseInt(sc.nextLine());
+        while(true){
+            if (m==0) {System.out.println(n); break;}
+            if (n==0) {System.out.println(m); break;}
+            if(n>m) n %=m;
+            else m %=n;
+        }
+    }
+
+    public static void math3 (){
+        int n=Integer.parseInt(sc.nextLine());
+        int i=2, k=4;
+        while(k<=n)
+        {
+            while(n%i==0)
+            {
+                System.out.print(i+" ");
+                n=n/i;
+            }
+            k = k + 2*i + 1;
+            i++;
+        }
+        if(n>1) System.out.print(n);
+    }
+
     public static void main(String[] args) {
 
         int LenghtArray = sc.nextInt();
@@ -69,6 +109,9 @@ public static void t1(int[] myArray)
         //t4(myArray);
         //t5(myArray);
         //t6(myArray);
+       // math1();
+        //math2();
+        math3();
     }
 
 }
